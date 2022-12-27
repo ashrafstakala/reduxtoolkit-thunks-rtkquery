5c7e8a923385ae551f9abc616b316a009d8acd39
ashrafstakala <ashrafstakala@gmail.com>
2022-12-27 18:22:48 +0200

    add skeleton loader with animation

--- src/store/thunks/fetchUsers.js
+++ src/store/thunks/fetchUsers.js
@@ -5,7 +5,7 @@ const fetchUsers = createAsyncThunk('users/fetch', async () => {
   const response = await axios.get('http://localhost:3005/users');
 
   // Use pause in DEV only
-  await pause(1000);
+  await pause(10000);
 
   return response.data;
 });
