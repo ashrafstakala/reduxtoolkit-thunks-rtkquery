8c226688981a0513c369c8df10753e967d9bd9f7
ashrafstakala <ashrafstakala@gmail.com>
2022-12-27 17:54:19 +0200

    add loading pause for dev

--- src/store/thunks/fetchUsers.js
+++ src/store/thunks/fetchUsers.js
@@ -4,7 +4,17 @@ import axios from 'axios';
 const fetchUsers = createAsyncThunk('users/fetch', async () => {
   const response = await axios.get('http://localhost:3005/users');
 
+  // Use pause in DEV only
+  await pause(1000);
+
   return response.data;
 });
+
+// USE IN DEV ONLY
+const pause = (duration) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, duration);
+  });
+};
 
 export { fetchUsers };
